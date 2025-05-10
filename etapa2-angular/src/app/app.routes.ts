c877ff58480748753d3b81ae8bd54c8cbfe62940
leticia <leticiaribeirosilva113@gmail.com>
2025-05-10 18:59:59 -0300

    Atualiza rotas para incluir criar-noticia e Home

--- etapa2-angular/src/app/app.routes.ts
+++ etapa2-angular/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DetalhesComponent } from './components/detalhes/detalhes.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+      // rota inicial
+  { path: '', component: HomeComponent },
+
+  // rota de detalhes com parâmetro de ID
+  { path: 'detalhes/:id', component: DetalhesComponent }
+];
+
