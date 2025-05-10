935ba967c658f2a998a1bbdc140f0665ac6479f9
leticia <leticiaribeirosilva113@gmail.com>
2025-05-10 18:54:54 -0300

    Adiciona component Detalhes para exibir detalhes da notícia

---
+++ etapa2-angular/src/app/components/detalhes/detalhes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detalhes',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detalhes.component.html'
+})
+export class DetalhesComponent implements OnInit {
+  id!: number;
+  post: any;
+
+  constructor(private route: ActivatedRoute, private postService: PostService) {}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.postService.getPostById(this.id).subscribe({
+      next: (data) => this.post = data,
+      error: () => console.error('Erro ao buscar o post')
+    });
+  }
+}
